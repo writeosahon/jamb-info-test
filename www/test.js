83206ae20d9e7662c4caef8c84fad37faae483b6
Osahon Okungbowa <writeosahon@yahoo.co.uk>
2018-01-06 12:17:49 +0100

    2nd commit

---
+++ www/test.js
@@ -0,0 +1,21 @@
+/**
+ * Created by UTOPIA SOFTWARE on 06/01/2018.
+ */
+
+function loadAds(){
+    console.log("FUNCTION CALLED");
+
+    // config banner ad
+    admob.banner.config({
+        id: 'ca-app-pub-8042133793597643/7566158747',
+        isTesting: true,
+        autoShow: true,
+        bannerAtTop: true,
+        overlap: true
+    });
+
+    // create banner ad
+    admob.banner.prepare().then(function(){console.log("AD LOADED")});
+}
+
+document.addEventListener("deviceready", loadAds);
