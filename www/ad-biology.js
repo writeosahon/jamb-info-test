063ce55fbfd8acc49c82477d56f2122a11c18d51
Osahon Okungbowa <writeosahon@yahoo.co.uk>
2018-01-07 09:45:27 +0100

    5th commit

---
+++ www/ad-biology.js
@@ -0,0 +1,45 @@
+/**
+ * Created by UTOPIA SOFTWARE on 06/01/2018.
+ */
+
+function loadAds(){
+    console.log("FUNCTION CALLED");
+
+    // config banner ad
+    admob.banner.config({
+        id: 'ca-app-pub-3940256099942544/6300978111',
+        isTesting: true,
+        autoShow: true,
+        bannerAtTop: true,
+        overlap: false
+    });
+
+    // config interstitial ad
+    admob.interstitial.config({
+        id: 'ca-app-pub-3940256099942544/1033173712',
+        isTesting: true,
+        autoShow: false
+    });
+
+    // create banner ad
+    admob.banner.prepare().then(function(){console.log("AD LOADED")});
+
+    // create interstitial ad
+    admob.interstitial.prepare().then(function(){console.log("INTERSTITIAL AD LOADED")});
+
+    $(function(){
+        $(document).on("click", '.btn.btn-success', function(){
+            console.log("BUTTON CLICKED");
+            admob.interstitial.isReady().then(function(){
+                admob.interstitial.show();
+            }).catch();
+        });
+    });
+
+    document.addEventListener("backbutton", function(){
+        window.history.back();
+    }, false);
+
+}
+
+document.addEventListener("deviceready", loadAds);
