c6df55141ce508e544aff71a0485570675660aec
writeosahon <wrwiteosahon@yahoo.co.uk>
2019-07-10 17:36:30 +0100

    9th commit

--- www/advert-1.js
+++ www/advert-1.js
@@ -80,7 +80,7 @@ function loadProducts(){
     console.log("STORE REGISTER BEGIN");
     // REGISTER THE PREMIUM JAMB Q&A PRODUCT WITH THE STORE OBJECT
     store.register({
-        id: "com.exams.examseriesjamb.premium.jamb.qa",
+        id: "com.exams.examseriesjamb.premium.jamb.qa1",
         alias: "premium jamb q&a",
         type: store.NON_CONSUMABLE
     });
