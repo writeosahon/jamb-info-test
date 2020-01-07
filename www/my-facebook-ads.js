2b129da3f430d430fc44aac1cc6149ebb1f12721
writeosahon <wrwiteosahon@yahoo.co.uk>
2020-01-07 13:38:39 +0100

    16th commit

--- www/my-facebook-ads.js
+++ www/my-facebook-ads.js
@@ -7,10 +7,8 @@
 function loadFacebookAds(){
 
     cordova.plugins.codeplayfacebookads.showBannerAds({
-        adId:"737711282985878_2732872996803020",
-        isTesting:true,
-        position:FacebookAds.AD_POSITION.BOTTOM_CENTER,
-        autoShow:true
+        bannerid:"737711282985878_2732872996803020",
+        isTesting:true
     },function(successResult){
         console.log("SUCCESS", successResult);
     },function (failureResult) {
